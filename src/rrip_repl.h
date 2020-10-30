58f5d24e9028bbe98efda60c4a85c88cfc5f7e74
tiandi03 <tiandi03@kuaishou.com>
2020-10-31 06:59:06 +0800

    another imple

--- src/rrip_repl.h
+++ src/rrip_repl.h
@@ -8,7 +8,6 @@ class SRRIPReplPolicy : public ReplPolicy {
     protected:
         // add class member variables here
         uint32_t *array;
-        bool *valid;
         uint32_t numLines;
         uint64_t rpvMax;
 
@@ -16,10 +15,8 @@ class SRRIPReplPolicy : public ReplPolicy {
         // add member methods here, refer to repl_policies.h
         SRRIPReplPolicy(uint32_t _numLines, uint32_t _rpvMax) : numLines(_numLines), rpvMax(_rpvMax) {
             array = gm_calloc<uint32_t>(numLines);
-            valid = gm_calloc<bool>(numLines);
             for(uint32_t i = 0; i < numLines; i++) {
-                array[i] = 0;
-                valid[i] = false;
+                array[i] = rpvMax+1;
             }
         }
 
@@ -29,30 +26,24 @@ class SRRIPReplPolicy : public ReplPolicy {
         }
 
         void update(uint32_t id, const MemReq* req) {
-            if(valid[id]) {
-                array[id] = 0;
-            } else {
-                array[id] = rpvMax-1;
-            }
+            array[id] = array[id] == rpvMax+1 ? rpvMax-1 : 0;
         }
 
         virtual void replaced(uint32_t id) {
-            valid[id] = false;
+            array[id] = rpvMax+1;
         }
 
         template <typename C> inline uint32_t rank(const MemReq* req, C cands) {
             uint32_t maxRRPV = -1;
             uint32_t maxId = -1;
             for (auto ci = cands.begin(); ci != cands.end(); ci.inc()) {
-                bool validity = valid[*ci];
                 uint32_t currRRPV = array[*ci];
-                if(validity && (currRRPV > maxRRPV)) {
+                if(currRRPV > maxRRPV) {
                     maxRRPV = currRRPV;
                     maxId = *ci;
                 }
             }
-            if(currRRPV < 0) panic("invalid rank call");
-            uint32_t delta = rpvMax - maxRRPV;
+            uint32_t delta = maxRRPV > rpvMax ? 0 : rpvMax-maxRRPV;
             for(uint32_t i = 0; i < numLines; i++) {
                 array[i] += delta;
             }
